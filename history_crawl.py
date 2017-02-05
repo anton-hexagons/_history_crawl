f07139d901620a1081b4bfb319faff8df32d1a0f
_ <antonheestand@Antons-MacBook-Pro.local>
2017-02-05 22:35:28 +0100

    adds caption info for images

--- history_crawl.py
+++ history_crawl.py
@@ -7,28 +7,28 @@ import urllib.request
 from html.parser import HTMLParser
 
 
-# if sys.argv.length > 2:
-# 	print('[depth]')
-# 	exit()
-
 wiki_history_url = '/wiki/History'
 max_depth = int(sys.argv[1]) if len(sys.argv) > 1 else 0
 max_depth_reached = False
 
-# img_min_res = 100
+img_min_res = 100
 
 page_url_list = []
 img_page_url_list = []
-# img_black_list = []
+img_other_black_list = []
 url_black_list = ['#', '/wiki/Wikipedia:Protection_policy#semi', '#mw-head', '#p-search', '/wiki/List_of_national_legal_systems']
 url_type_black_list = ['license']
 
 
 in_content = False
-img_page_urls = []
-# page_imgs = []
-page_sub_url_packs = []
 page_thumbinner = False
+page_thumbcaption = False
+page_thumbcaption_magnify = False
+img_page_urls = []
+img_caption = ''
+img_captions = []
+# img_other_urls = []
+page_sub_url_packs = []
 # heading_title = ''
 # hit_heading = False
 
@@ -36,10 +36,12 @@ class HTMLParser__wiki_page(HTMLParser):
 
 	def handle_starttag(self, tag, attrs):
 		global in_content
-		global img_page_urls
-		# global page_imgs
-		global page_sub_url_packs
 		global page_thumbinner
+		global page_thumbcaption
+		global page_thumbcaption_magnify
+		global img_page_urls
+		# global img_other_urls
+		global page_sub_url_packs
 		global hit_heading
 
 		## page imgs from thumbinners
@@ -55,12 +57,20 @@ class HTMLParser__wiki_page(HTMLParser):
 				if attr[0] == 'class':
 					if attr[1] == 'thumbinner':
 						page_thumbinner = True
-		if page_thumbinner and tag == 'a':
+						return
+					if attr[1] == 'thumbcaption':
+						page_thumbcaption = True
+						return
+					if page_thumbcaption:
+						if attr[1] == 'magnify':
+							page_thumbcaption_magnify = True
+							return
+		if tag == 'a':
 			for attr in attrs:
 				if attr[0] == 'href':
-					img_page_url = 'https://en.wikipedia.org' + attr[1]
-					img_page_urls.append(img_page_url)
-			page_thumbinner = False
+					if page_thumbinner:
+						img_page_urls.append('https://en.wikipedia.org' + attr[1])
+						return
 
 		# if tag == 'h1':
 		# 	for attr in attrs:
@@ -68,28 +78,27 @@ class HTMLParser__wiki_page(HTMLParser):
 		# 			if attr[1] == 'firstHeading':
 		# 				hit_heading = True
 		
-		## page img
-		# if tag == 'img':
-		# 	page_img = {
-		# 		'title': None,
-		# 		'url': None,
-		# 		'width': None,
-		# 		'height': None
-		# 	}
-		# 	for attr in attrs:
-		# 		if attr[0] == 'alt':
-		# 			if attr[1]:
-		# 				page_img['title'] = attr[1]
-		# 		elif attr[0] == 'src':
-		# 			page_img['url'] = attr[1][2:]
-		# 		elif attr[0] == 'width':
-		# 			page_img['width'] = int(attr[1])
-		# 		elif attr[0] == 'height':
-		# 			page_img['height'] = int(attr[1])
-		# 	if page_img['url']:
-		# 		if page_img['width'] >= img_min_res and page_img['height'] >= img_min_res:
-		# 			if page_img['url'] not in img_black_list:
-		# 				page_imgs.append(page_img)
+		if tag == 'img':
+			if page_thumbinner:
+				page_thumbinner = False
+		# 	else:
+		# 		page_img = {
+		# 			'url': None,
+		# 			'width': None,
+		# 			'height': None
+		# 		}
+		# 		for attr in attrs:
+		# 			if attr[0] == 'src':
+		# 				page_img['url'] = 'https://' + attr[1][2:]
+		# 			elif attr[0] == 'width':
+		# 				page_img['width'] = int(attr[1])
+		# 			elif attr[0] == 'height':
+		# 				page_img['height'] = int(attr[1])
+		# 		# if page_img['url']:
+		# 		if page_img['width'] > img_min_res and page_img['height'] > img_min_res:
+		# 			if page_img['url'] not in img_other_black_list:
+		# 				img_other_urls.append(page_img['url'])
+
 		lang_target = False
 		if tag == 'a':
 			for attr in attrs:
@@ -101,7 +110,7 @@ class HTMLParser__wiki_page(HTMLParser):
 			## page url
 			if tag == 'a':
 				page_url_pack = {
-					'title': None,
+					'title': '',
 					'url': None
 				}
 				url_type = None
@@ -119,15 +128,55 @@ class HTMLParser__wiki_page(HTMLParser):
 								page_sub_url_packs.append(page_url_pack)
 								return
 
-	# def handle_data(self, data):
-	# 	global hit_heading
-	# 	global heading_title
-	# 	if hit_heading:
-	# 		heading_title = data
-	# 		hit_heading = False
+	def handle_data(self, data):
+		global page_thumbcaption
+		global img_caption
 
-	# def handle_endtag(self, tag):
-	# 	pass
+		if page_thumbcaption:
+			img_caption += data
+
+		# global hit_heading
+		# global heading_title
+		# if hit_heading:
+		# 	heading_title = data
+		# 	hit_heading = False
+
+	def handle_endtag(self, tag):
+		global page_thumbcaption_magnify
+		global page_thumbcaption
+		global img_caption
+		global img_captions
+
+		if tag == 'div':
+			if page_thumbcaption_magnify:
+				page_thumbcaption_magnify = False
+			elif page_thumbcaption:
+				img_captions.append(img_caption)
+				img_caption = ''
+				page_thumbcaption = False
+
+
+def saveImg(img_url, img_file, wiki_img_folder):
+	if img_url in img_page_url_list:
+		print('img already saved globally:', img_url)
+		return
+	if len(img_file) > 100:
+		img_file_name = '.'.join(img_file.split('.')[:-1])
+		img_file_ext = img_file.split('.')[-1]
+		img_file = img_file_name[:100] + '__.' + img_file_ext
+		print('img file name too long, cropped at char 100, new file name:', img_file)
+	if not os.path.exists(wiki_img_folder):
+		os.makedirs(wiki_img_folder)
+	img_file_path = os.path.join(wiki_img_folder, img_file)
+	img_page_data['path'] = img_file_path
+	if os.path.exists(img_file_path):
+		print('img already saved, url:', img_url)
+		return
+	img_data = urllib.request.urlopen('https://' + img_page_data['url']).read()
+	img_file = open(img_file_path, 'wb')
+	img_file.write(img_data)
+	img_page_url_list.append(img_url)
+	print('img saved, url:', img_url)
 
 
 hit_org_img = False
@@ -170,22 +219,9 @@ class HTMLParser__wiki_img_page(HTMLParser):
 					img_page_data['url'] = attr[1][2:]
 			hit_org_img = False
 			img_file = img_page_data['url'].split('/')[-1].replace('File:', '')
-			if len(img_file) > 100:
-				img_file_name = '.'.join(img_file.split('.')[:-1])
-				img_file_ext = img_file.split('.')[-1]
-				img_file = img_file_name[:100] + '.' + img_file_ext
-				print('file name too long, cropped at 255, new file name:', img_file)
-			img_file_folder = os.path.join(wiki_folder, 'imgs')
-			if not os.path.exists(img_file_folder):
-				os.makedirs(img_file_folder)
-			img_file_path = os.path.join(img_file_folder, img_file)
+			saveImg('https://' + img_page_data['url'], img_file, os.path.join(wiki_folder, 'imgs'))
+			img_file_path = os.path.join(wiki_folder, 'imgs', img_file)
 			img_page_data['path'] = img_file_path
-			if os.path.exists(img_file_path):
-				print('img already saved')
-				return
-			img_data = urllib.request.urlopen('https://' + img_page_data['url']).read()
-			img_file = open(img_file_path, 'wb')
-			img_file.write(img_data)
 			return
 		
 		if tag == 'time':
@@ -200,31 +236,6 @@ class HTMLParser__wiki_img_page(HTMLParser):
 					date['date'] = attr[1]
 			img_page_data['date'] = date
 			return
-
-
-		# ## img
-		# if tag == 'img':
-		# 	img = {
-		# 		'title': None,
-		# 		'url': None,
-		# 		'width': None,
-		# 		'height': None
-		# 	}
-		# 	rel = None
-		# 	for attr in attrs:
-		# 		if attr[0] == 'alt':
-		# 			if attr[1]:
-		# 				img['title'] = attr[1]
-		# 		elif attr[0] == 'src':
-		# 			img['url'] = attr[1][2:]
-		# 		elif attr[0] == 'width':
-		# 			img['width'] = int(attr[1])
-		# 		elif attr[0] == 'height':
-		# 			img['height'] = int(attr[1])
-		# 	if img['url']:
-		# 		if img['width'] >= 100 and img['height'] >= 100:
-		# 			if img['url'] not in img_black_list:
-		# 				page_imgs.append(img)
 
 	def handle_data(self, data):
 		# global hit_description
@@ -285,7 +296,8 @@ def Crawl__wiki_page(url_pack, wiki_sub_folder):
 	global max_depth
 	global max_depth_reached
 	global img_page_urls
-	# global page_imgs
+	global img_captions
+	global img_other_urls
 	global page_sub_url_packs
 	global img_page_data
 	global heading_title
@@ -336,6 +348,14 @@ def Crawl__wiki_page(url_pack, wiki_sub_folder):
 		print('page_does_not_exist, sub folder:', wiki_sub_folder)
 		return
 
+	if 'disambiguation' in url_pack['title']:
+		print('disambiguation in title, url:', url_pack['url'])
+		return
+
+	if 'ftp' in url_pack['url']:
+		print('ftp url:', url_pack['url'])
+		return
+
 	# if '\\\\' in url_pack['title']:
 	# 	print('decode:', url_pack['title'], '<<<<<<<<<<')
 	# 	exit()
@@ -348,7 +368,7 @@ def Crawl__wiki_page(url_pack, wiki_sub_folder):
 	wiki_page_data = {
 		'title': url_pack['title'],
 		'url': 'https://en.wikipedia.org' + url_pack['url'],
-		'img_url': [],
+		'img_packs': [],
 		# 'imgs': [],
 		'sub_url_packs': []
 	}
@@ -356,51 +376,63 @@ def Crawl__wiki_page(url_pack, wiki_sub_folder):
 	if wiki_page_data['title'] == None:
 		wiki_page_data['title'] = ''
 
-	file = 'wiki__' + wiki_page_data['title'].replace(' ', '_') + '.json'
+	## path
+	json_file_name = 'wiki__' + wiki_page_data['title'].replace(' ', '_') + '.json'
 	pyFileFolder = os.path.dirname(os.path.realpath(__file__))
 	folder = os.path.join(pyFileFolder, 'wiki__History', wiki_sub_folder).replace(':', '__')
 	if not os.path.exists(folder):
 		os.makedirs(folder)
 	wiki_folder = folder
-	json_path = os.path.join(folder, file)
+	json_file_path = os.path.join(folder, json_file_name)
 
-	if not os.path.exists(json_path):
 
-		wiki_page = str(urllib.request.urlopen(wiki_page_data['url']).read())[2:-1]
-		wiki_page_parser = HTMLParser__wiki_page()
-		wiki_page_parser.feed(wiki_page)
-		wiki_page_data['sub_url_packs'] = page_sub_url_packs
-		# wiki_page_data['title'] = heading_title
-		# heading_title = ''
+	# if not os.path.exists(json_file_path):
 
-		# wiki_page_data['imgs'] = page_imgs
-		# for page_img in wiki_page_data['imgs']:
-		# 	pass
+	## wiki url parse
+	wiki_page = str(urllib.request.urlopen(wiki_page_data['url']).read())[2:-1]
+	wiki_page_parser = HTMLParser__wiki_page()
+	wiki_page_parser.feed(wiki_page)
+	wiki_page_data['sub_url_packs'] = page_sub_url_packs
+	page_sub_url_packs = []
+	
+	## wiki imgs parse
+	for i in range(len(img_page_urls)):
+		Crawl__wiki_img_page(img_page_urls[i])
+		img_page_data['page_url'] = img_page_urls[i]
+		if len(img_page_urls) == len(img_captions):
+			img_page_data['img_caption'] = img_captions[i]
+		wiki_page_data['img_packs'].append(img_page_data)
+	img_page_urls = []
+	img_captions = []
 
-		for img_page_url in img_page_urls:
-			if img_page_url in img_page_url_list:
-				print('img already saved:', img_page_url)
-				break
-			print('img', img_page_url)
-			Crawl__wiki_img_page(img_page_url)
-			img_page_data['page_url'] = img_page_url
-			wiki_page_data['img_url'].append(img_page_data)
+	## save json
+	with open(json_file_path, 'w') as outfile:
+		print(json.dumps(wiki_page_data, sort_keys=False, indent=4), file=outfile)
+	
+	## save html
+	html_file_name = 'wiki__' + wiki_page_data['title'].replace(' ', '_') + '.html'
+	html_file_path = os.path.join(folder, html_file_name)
+	with open(html_file_path, 'w') as outfile:
+		print(wiki_page, file=outfile)
 
-		img_page_urls = []
+	# ## wiki other imgs
+	# for img_other_url in img_other_urls:
+	# 	img_file = img_other_url.split('/')[-1]
+	# 	wiki_img_folder = os.path.join(wiki_folder, 'imgs')
+	# 	saveImg(img_other_url, img_file, wiki_img_folder) #_
+	
+	print('url parsed')
 
-		with open(json_path, 'w') as outfile:
-			print(json.dumps(wiki_page_data, sort_keys=False, indent=4), file=outfile)
+	# else:
+	# 	wiki_page_data = json.loads(open(json_file_path).read()) #.strip("'<>() ")
+	# 	print('url already parsed, loaded json. url:', url_pack['url'])
 
-	else:
-		wiki_page_data = json.loads(open(json_path).read()) #.strip("'<>() ")
-		print('url already parsed, loaded json. url:', url_pack['url'])
 
 	page_url_list.append(url_pack['url'])
 	
+	## wiki sub urls
 	print('sub urls:', len(wiki_page_data['sub_url_packs']))
 	for sub_url_pack in wiki_page_data['sub_url_packs']:
-		if sub_url_pack['title'] == None:
-			sub_url_pack['title'] = ''
 		sub_folder = os.path.join(wiki_sub_folder, 'sub_wikis', 'wiki__' + sub_url_pack['title'].replace(' ', '_'))
 		Crawl__wiki_page(sub_url_pack, sub_folder)
 
@@ -410,4 +442,4 @@ Crawl__wiki_page({
 	'url': wiki_history_url
 }, '')
 
-print('History Crawl done!')
+print('History Crawl done!  Layer depth:', max_depth, ' Links:', len(page_url_list), ' New images:', len(img_page_url_list))
